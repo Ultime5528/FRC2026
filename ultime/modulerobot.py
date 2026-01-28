280f4d8b2509688146bc841b93a9432fd5c32e49
Vincent Benoit <4100046@keranna.qc.ca>
2026-01-28 18:08:11 -0500

    WIP

--- ultime/modulerobot.py
+++ ultime/modulerobot.py
@@ -1,12 +1,16 @@
 from enum import Enum, auto
+from queue import PriorityQueue
 from typing import Final
 
 import commands2
 import hal
 import wpilib
-from wpilib import RobotBase, DSControlWord, Watchdog
+from ntcore import NetworkTable, NetworkTableInstance
+from wpilib import RobotBase, DSControlWord, Watchdog, SmartDashboard, RobotController
 
 from ultime.module import ModuleList, Module
+
+is_simulation = wpilib.RobotBase.isSimulation()
 
 
 class ModuleRobot(wpilib.RobotBase):
@@ -24,15 +28,15 @@ class ModuleRobot(wpilib.RobotBase):
         self.modules = ModuleList()
         self._called_ds_connected = False
         self._last_mode = ModuleRobot.Mode.kNone
-        self._watchdog = Watchdog(self.period, self.printLoopOverRunMessage())
+        self._watchdog = Watchdog(self.period, lambda: None)
 
     def _loopFunc(self):
         wpilib.DriverStation.refreshData()
-        ds_control_world = wpilib.DSControlWord()
+        self._watchdog.reset()
+        # ds_control_world = wpilib.DSControlWord()
 
-        #reset watchdog
         is_enabled, is_autonomous, is_test = self.getControlState()
-        mode = ModuleRobot.Mode.kNone
+
         if not is_enabled:
             mode = ModuleRobot.Mode.kDisable
         elif is_autonomous:
@@ -42,14 +46,14 @@ class ModuleRobot(wpilib.RobotBase):
         else:
             mode = ModuleRobot.Mode.kTeleop
 
-        #à ajouter si nous voulons mettre une fonction qui est appelée quand la DS se connecte
-        #if not self.called_ds_connected and ds_control_world.isDSAttached():
+        # à ajouter si nous voulons mettre une fonction qui est appelée quand la DS se connecte
+        # if not self.called_ds_connected and ds_control_world.isDSAttached():
         #    self.called_ds_connected = True
         #    self.driveStationConnected()
 
-        #si le mode change, appeler les fonctions de sortie et d'entrée
+        # si le mode change, appeler les fonctions de sortie et d'entrée
         if self._last_mode is not mode:
-            #fonctions de sortie
+            # fonctions de sortie
             if self._last_mode is ModuleRobot.Mode.kDisable:
                 self.disabledExit()
             elif self._last_mode is ModuleRobot.Mode.kAutonomous:
@@ -59,44 +63,78 @@ class ModuleRobot(wpilib.RobotBase):
             elif self._last_mode is ModuleRobot.Mode.kTest:
                 self.testExit()
 
-            #fonctions d'entrée
+            # fonctions d'entrée
             if mode is ModuleRobot.Mode.kDisable:
                 self.disabledInit()
+                self._watchdog.addEpoch("DisabledInit()")
             elif mode is ModuleRobot.Mode.kAutonomous:
                 self.autonomousInit()
+                self._watchdog.addEpoch("AutonomousInit()")
             elif mode is ModuleRobot.Mode.kTeleop:
                 self.teleopInit()
+                self._watchdog.addEpoch("TeleopInit()")
             elif mode is ModuleRobot.Mode.kTest:
                 self.testInit()
+                self._watchdog.addEpoch("TestInit()")
 
             self._last_mode = mode
 
-        #TODO appeler IO update inputs
+        # TODO appeler IO update inputs
 
-
-        #appeler les fonctions correspondantes au mode du robot
+        # appeler les fonctions correspondantes au mode du robot
         if mode is ModuleRobot.Mode.kDisable:
             hal.observeUserProgramDisabled()
             self.disabledPeriodic()
+            self._watchdog.addEpoch("DisabledPeriodic()")
         elif mode is ModuleRobot.Mode.kAutonomous:
             hal.observeUserProgramAutonomous()
             self.autonomousPeriodic()
+            self._watchdog.addEpoch("AutonomousPeriodic()")
         elif mode is ModuleRobot.Mode.kTeleop:
             hal.observeUserProgramTeleop()
             self.teleopPeriodic()
+            self._watchdog.addEpoch("TeleopPeriodic()")
         elif mode is ModuleRobot.Mode.kTest:
             hal.observeUserProgramTest()
             self.testPeriodic()
+            self._watchdog.addEpoch("TestPeriodic()")
 
         self.robotPeriodic()
+        self._watchdog.addEpoch("RobotPeriodic()")
 
-        #TODO Scheduler run
+        # TODO Scheduler run
 
-        #TODO log everything
+        # TODO log everything
 
-        #TODO appeler IO apply outputs
+        # TODO appeler IO apply outputs
 
+        SmartDashboard.updateValues()
+        self._watchdog.addEpoch("SmartDashboard.updateValues()")
 
+        if is_simulation:
+            hal.simPeriodicBefore()
+            self.simulationPeriodic()
+            hal.simPeriodicAfter()
+            self._watchdog.addEpoch("SimulationPeriodic()")
+
+        self._watchdog.disable()
+
+        NetworkTableInstance.getDefault().flushLocal()
+
+        if self._watchdog.isExpired():
+            wpilib.reportError(f"Loop time  of {self.period}s overrun")
+            self._watchdog.printEpochs()
+
+    def startCompetition(self):
+        self.robotInit()
+
+        if is_simulation:
+            self.simulationInit()
+
+        hal.observeUserProgramStarting()
+
+        while True:
+            pass
 
 
     def addModules(self, *modules: Module):
@@ -116,6 +154,12 @@ class ModuleRobot(wpilib.RobotBase):
 
         if RobotBase.isSimulation():
             self.modules.simulationPeriodic()
+
+    def simulationInit(self):
+        self.modules.simulationInit()
+
+    def simulationPeriodic(self):
+        self.modules.simulationPeriodic()
 
     def disabledInit(self):
         self.modules.disabledInit()
@@ -155,6 +199,3 @@ class ModuleRobot(wpilib.RobotBase):
 
     def driverStationConnected(self):
         self.modules.driverStationConnected()
-
-    def printLoopOverRunMessage(self):
-        wpilib.reportError(f"Loop time  of {self.period}s overrun")
