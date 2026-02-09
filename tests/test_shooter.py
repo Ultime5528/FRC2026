8428511c8afd350dec8f631aa341d5ee6780ee27
Edmonton <4201901@keranna.qc.ca>
2026-02-09 17:58:10 -0500

    Add tests for prepare_shoot and shoot

--- tests/test_shooter.py
+++ tests/test_shooter.py
@@ -1,6 +1,8 @@
 from pytest import approx
 
 from commands.shooter.manualshoot import ManualShoot
+from commands.shooter.prepareshoot import PrepareShoot
+from commands.shooter.shoot import Shoot
 from robot import Robot
 from ultime.tests import RobotTestController
 
@@ -33,3 +35,51 @@ def test_ManualShoot(robot_controller: RobotTestController, robot: Robot):
     robot_controller.wait(10.0)
 
     assert cmd.speed_rpm == approx(shooter.getCurrentSpeed(), abs=10.0)
+
+
+def test_prepareShoot(robot_controller: RobotTestController, robot: Robot):
+    shooter = robot.hardware.shooter
+
+    robot_controller.startTeleop()
+
+    assert shooter._flywheel_controller.getSetpoint() == 0
+    assert shooter._indexer.get() == 0
+    assert shooter._feeder.get() == 0
+
+    cmd = PrepareShoot(shooter)
+    cmd.schedule()
+    robot_controller.wait_one_frame()
+
+    assert cmd.isScheduled()
+
+    assert shooter._flywheel_controller.getSetpoint() > 0
+    assert shooter._indexer.get() == 0
+    assert shooter._feeder.get() == 0
+
+    robot_controller.wait(10.0)
+
+    assert shooter._flywheel_controller.getSetpoint() == approx(shooter.getCurrentSpeed(), abs=10.0)
+
+def test_shoot(robot_controller: RobotTestController, robot: Robot):
+    shooter = robot.hardware.shooter
+
+    robot_controller.startTeleop()
+
+    assert shooter._flywheel_controller.getSetpoint() == 0
+    assert shooter._indexer.get() == 0
+    assert shooter._feeder.get() == 0
+
+    cmd = Shoot(shooter)
+    cmd.schedule()
+    robot_controller.wait_one_frame()
+
+    assert cmd.isScheduled()
+
+    assert shooter._flywheel_controller.getSetpoint() > 0
+
+    robot_controller.wait_until(shooter.isAtVelocity, 10.0)
+
+    assert shooter._indexer.get() == approx(shooter.speed_indexer)
+    assert shooter._feeder.get() == approx(shooter.speed_feeder)
+
+
