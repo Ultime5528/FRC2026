3994e9b4b186a99ec750508971cb75e2a406ac59
HayderChammakhi <4101192@keranna.qc.ca>
2026-02-11 19:48:55 -0500

    stuff for rpm actually uses the shooter pose

--- modules/shootercalcmodule.py
+++ modules/shootercalcmodule.py
@@ -2,7 +2,7 @@ import math
 
 from wpilib import DriverStation
 from wpimath import units
-from wpimath.geometry import Rotation2d, Translation3d, Pose3d, Rotation3d
+from wpimath.geometry import Rotation2d, Translation3d, Pose3d, Rotation3d, Transform3d
 
 from subsystems.drivetrain import Drivetrain
 from ultime.autoproperty import autoproperty
@@ -15,23 +15,23 @@ blue_hub = Translation3d(11.915394, 4.034536, 3.057144)
 
 def computeRobotRotationToAlign(
     robot_pose3d: Pose3d,
-    shooter_offset: Pose3d,
-    shooter_extremity: Translation3d,
+    shooter_offset: Transform3d,
     hub_pose: Translation3d,
 ) -> Rotation2d:
 
+    shooter_extremity = Translation3d((math.cos(shooter_offset.rotation().y)+shooter_offset.translation().x),(math.sin(shooter_offset.rotation().y)+shooter_offset.translation().y), 2)
+
     delta_hub_and_bot = hub_pose - robot_pose3d.translation()
     hub_at_origin = delta_hub_and_bot.rotateBy(-robot_pose3d.rotation())
-    shooter_direction = shooter_extremity - shooter_offset
+    shooter_direction = shooter_extremity - shooter_offset.translation()
 
     A = shooter_direction.cross(hub_at_origin)
     B = shooter_direction.dot(hub_at_origin)
-    C = shooter_offset.cross(shooter_extremity)
+    C = shooter_offset.translation().cross(shooter_extremity)
 
     denominator = (A**2) + (B**2)
 
 
-    robot_pose3d.transformBy()
     # to avoid domain errors
     if abs(C / denominator) > 1 or denominator == 0:
         return Rotation2d()
@@ -56,8 +56,10 @@ def computeShooterSpeedToShoot(
     delta_y = target_position.y - shooter_position.y
     delta_z = target_position.z - target_position.z
 
-    distance_xy_squared = (delta_x**2)+(delta_y**2)
-    distance_xy = math.sqrt(distance_xy_squared)
+    distance_xy = math.hypot(delta_x,delta_y)
+
+    distance_xy_squared = distance_xy**2
+
 
     numerator = gravity * distance_xy_squared
     denominator = (2 * (math.cos(shooter_angle)) ** 2) * (
@@ -78,12 +80,15 @@ def shouldUseGuide(
     else:
         return False
 
+def computeShooterPosition(robot_pose: Pose3d, shooter_pose: Transform3d) -> Translation3d:
+    return (robot_pose.transformBy(shooter_pose)).translation()
+
 
 class ShooterCalcModule(Module):
     long_zone = autoproperty(6.0)
     red_hub = Translation3d(4.625594, 4.034536, 3.057144)
     blue_hub = Translation3d(11.915394, 4.034536, 3.057144)
-    shooter_offset = Pose3d(-0.6,-0.5,0.0,Rotation3d(0,180,0))
+    shooter_offset = Transform3d(-0.6,-0.5,0.0,Rotation3d(0,180,0))
     speed_guide_open = autoproperty([4.0, 6.0, 7.0, 9.5, 11.0, 14.0])
     rpm_guide_open = autoproperty([501.24, 751.86, 877.17, 1190.445, 1378.41, 1754.34])
     speed_guide_closed = autoproperty([3.5, 5.0, 5.5, 7.0, 9.0, 11.5])
@@ -104,14 +109,15 @@ class ShooterCalcModule(Module):
             self.speed_guide_closed, self.rpm_guide_closed
         )
 
-
-
-    e = (Pose3d(red_hub, Rotation3d())).
-
     def robotPeriodic(self) -> None:
         self.shooter_position = self._ComputeShooterPosition()
         self.shooter_extremity_position = self._ComputeShooterExtremityPosition()
         self.hub_position = self._getTargetPose()
+
+    def _getShooterPose(self) -> Translation3d:
+        return computeShooterPosition(self._drivetrain.getPose(),self.shooter_offset)
+
+
 
     def _getTargetPose(self) -> Translation3d:
         if DriverStation.getAlliance() == DriverStation.Alliance.kRed:
@@ -123,7 +129,6 @@ class ShooterCalcModule(Module):
         return computeRobotRotationToAlign(
             self._drivetrain.getPose(),
             self.shooter_offset,
-            self.shooter_extremity,
             self._getTargetPose(),
         )
 
@@ -138,12 +143,12 @@ class ShooterCalcModule(Module):
         if self.shouldUseGuide():
             return self._interpolator_for_open_guide.interpolate(
                 computeShooterSpeedToShoot(
-                    self._drivetrain.getPose(), self._getTargetPose(), self.long_zone
+                    self._getShooterPose(), self._getTargetPose(), self.long_zone
                 )
             )
         else:
             return self._interpolator_for_closed_guide.interpolate(
                 computeShooterSpeedToShoot(
-                    self._drivetrain.getPose(), self._getTargetPose(), self.long_zone
+                    self._getShooterPose(), self._getTargetPose(), self.long_zone
                 )
             )
