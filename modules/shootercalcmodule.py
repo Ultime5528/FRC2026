4eac5272118a126abec5a6a8b9487ab765aa86e3
HayderChammakhi <4101192@keranna.qc.ca>
2026-02-18 20:05:58 -0500

    test works

--- modules/shootercalcmodule.py
+++ modules/shootercalcmodule.py
@@ -1,40 +1,49 @@
 import math
 
 from wpilib import DriverStation
-from wpimath import units
-from wpimath.geometry import Rotation2d, Translation3d, Pose3d, Rotation3d, Transform3d
+from wpimath.geometry import Translation3d, Pose3d, Rotation3d, Transform3d
 
 from subsystems.drivetrain import Drivetrain
 from ultime.autoproperty import autoproperty
 from ultime.linearinterpolator import LinearInterpolator
 from ultime.module import Module
 
-red_hub = Translation3d(4.625594, 4.034536, 3.057144)
-blue_hub = Translation3d(11.915394, 4.034536, 3.057144)
+
+def computeRobotRotationToAlignSimple(
+    shooter_pose3d: Pose3d,
+    hub_position: Translation3d,
+) -> float:
+    shooter_to_hub = (hub_position - shooter_pose3d.translation()).toTranslation2d()
+    shooter_to_hub_angle = shooter_to_hub.angle().radians()
+    shooter_angle = shooter_pose3d.rotation().angle
+    return shooter_to_hub_angle - shooter_angle
 
 
 def computeRobotRotationToAlign(
     robot_pose3d: Pose3d,
-    shooter_offset: Transform3d,
-    shooter_extremity: Translation3d,
+    shooter_offset_origin: Translation3d,
+    shooter_extremity_origin: Translation3d,
     hub_pose: Translation3d,
 ) -> float:
 
     delta_hub_and_bot = hub_pose - robot_pose3d.translation()
     hub_at_origin = delta_hub_and_bot.rotateBy(-robot_pose3d.rotation())
-    shooter_direction = shooter_extremity - shooter_offset.translation()
+    shooter_direction = shooter_extremity_origin - shooter_offset_origin
 
-    A = shooter_direction.cross(hub_at_origin)
-    B = shooter_direction.dot(hub_at_origin)
-    C = shooter_offset.translation().cross(shooter_extremity)
+    A = -(shooter_direction.x * hub_at_origin.y - shooter_direction.y * hub_at_origin.x)
+    B = -(shooter_direction.x * hub_at_origin.x + shooter_direction.y * hub_at_origin.y)
+    C = (
+        shooter_offset_origin.x * shooter_extremity_origin.y
+        - shooter_offset_origin.y * shooter_extremity_origin.x
+    )
 
-    denominator = (A**2) + (B**2)
+    denominator = math.sqrt((A**2) + (B**2))
 
     # to avoid domain errors
     if abs(C / denominator) > 1 or denominator == 0:
         return 0.0
     else:
-        return math.atan2(-B, -A) + math.acos(C / denominator)
+        return math.atan2(B, A) + math.acos(C / denominator)
 
 
 def computeShooterSpeedToShoot(
@@ -44,19 +53,18 @@ def computeShooterSpeedToShoot(
 ) -> float:
 
     if target_position.distance(shooter_position) >= long_shoot_zone:
-        shooter_angle = 60.0*(math.pi/180)
+        shooter_angle = math.radians(60.0)
     else:
-        shooter_angle = 70.0*(math.pi/180)
+        shooter_angle = 70.0 * (math.pi / 180)
 
     gravity = 9.80665
 
-    distance_shooter_xy = math.hypot(shooter_position.x,shooter_position.y)
-    distance_target_xy = math.hypot(target_position.x,target_position.y)
+    distance_shooter_xy = math.hypot(shooter_position.x, shooter_position.y)
+    distance_target_xy = math.hypot(target_position.x, target_position.y)
     delta_xy = distance_target_xy - distance_shooter_xy
     delta_z = target_position.z - shooter_position.z
 
-
-    distance_xy_squared = delta_xy ** 2
+    distance_xy_squared = delta_xy**2
 
     numerator = gravity * distance_xy_squared
     denominator = (2 * ((math.cos(shooter_angle)) ** 2)) * (
@@ -88,8 +96,10 @@ class ShooterCalcModule(Module):
     long_zone = autoproperty(6.0)
     red_hub = Translation3d(4.625594, 4.034536, 3.057144)
     blue_hub = Translation3d(11.915394, 4.034536, 3.057144)
-    shooter_offset = Transform3d(-0.1525, -0.271, 0.5, Rotation3d(Translation3d(0,0,1),0))
-    shooter_extremity = Transform3d(0.1525,-0.271,0.5,Rotation3d(Translation3d(0,0,1),0))
+    shooter_offset = Transform3d(
+        -0.1525, -0.271, 0.5, Rotation3d(Translation3d(0, 0, 1), 0)
+    )
+    shooter_extremity = Translation3d(0.1525, -0.271, 0.5)
     speed_guide_open = autoproperty([4.0, 6.0, 7.0, 9.5, 11.0, 14.0])
     rpm_guide_open = autoproperty([501.24, 751.86, 877.17, 1190.445, 1378.41, 1754.34])
     speed_guide_closed = autoproperty([3.5, 5.0, 5.5, 7.0, 9.0, 11.5])
@@ -113,24 +123,24 @@ class ShooterCalcModule(Module):
     def _getShooterPose(self) -> Translation3d:
         return computeShooterPosition(self._drivetrain.getPose(), self.shooter_offset)
 
-    def _getTargetPose(self) -> Translation3d:
+    def _getHubPosition(self) -> Translation3d:
         if DriverStation.getAlliance() == DriverStation.Alliance.kRed:
             return self.red_hub
         else:
             return self.blue_hub
 
-    def _getAngleToAlignWithTarget(self) -> float:
+    def getAngleToAlignWithTarget(self) -> float:
         return computeRobotRotationToAlign(
             self._drivetrain.getPose(),
-            self.shooter_offset,
+            self.shooter_offset.translation(),
             self.shooter_extremity,
-            self._getTargetPose(),
+            self._getHubPosition(),
         )
 
-    def shouldUseGuide(self) -> bool:
+    def _shouldUseGuide(self) -> bool:
         return shouldUseGuide(
             self._drivetrain.getPose().translation(),
-            self._getTargetPose(),
+            self._getHubPosition(),
             self.long_zone,
         )
 
@@ -138,12 +148,12 @@ class ShooterCalcModule(Module):
         if self.shouldUseGuide():
             return self._interpolator_for_open_guide.interpolate(
                 computeShooterSpeedToShoot(
-                    self._getShooterPose(), self._getTargetPose(), self.long_zone
+                    self._getShooterPose(), self._getHubPosition(), self.long_zone
                 )
             )
         else:
             return self._interpolator_for_closed_guide.interpolate(
                 computeShooterSpeedToShoot(
-                    self._getShooterPose(), self._getTargetPose(), self.long_zone
+                    self._getShooterPose(), self._getHubPosition(), self.long_zone
                 )
             )
--- tests/test_shootercalcmodule.py
+++ tests/test_shootercalcmodule.py
@@ -1,30 +1,51 @@
 import math
 
+from _pytest.python_api import approx
 from hal import AllianceStationID
 from wpilib.simulation import DriverStationSim
 from wpimath.geometry import Pose2d
 
-from modules.shootercalcmodule import ShooterCalcModule, computeRobotRotationToAlign
+from modules.shootercalcmodule import (
+    ShooterCalcModule,
+    computeRobotRotationToAlign,
+    computeShooterPosition,
+    computeRobotRotationToAlignSimple,
+)
 from commands.drivetrain.resetpose import ResetPose
 from robot import Robot
 from ultime.tests import RobotTestController
-from wpimath.geometry import Pose3d,Rotation3d,Translation3d,Transform3d
+from wpimath.geometry import Pose3d, Rotation3d, Translation3d, Transform3d
 
 
 # def test_ShooterPower(robot_controller: RobotTestController, robot: Robot):
 #     shooter_calc_module = ShooterCalcModule(robot.hardware.drivetrain)
 #     red_hub = shooter_calc_module.red_hub
 #     robot_controller.startTeleop()
-#     robot_controller.run_command(ResetPose(robot.hardware.drivetrain,Pose2d((red_hub.x)-4.0, (red_hub.y)-3.0)), 3.0)
+#     robot_controller.run_command(ResetPose(robot.hardware.drivetrain,Pose2d(2,3,0)),timeout=3.0)
 #     robot_controller.wait(0.2)
 #     angle = shooter_calc_module._getAngleToAlignWithTarget()
 #     rpm = shooter_calc_module.getRPM()
 #     assert
 
+
 def test_computeRobotRotationToAlign_1():
     robot = Pose3d(2, 3, 0, Rotation3d(Translation3d(0, 0, 1), 0))
-    offset = Transform3d(-0.1525, -0.271, 0.5, Rotation3d())
-    extrem = Translation3d(0.1525, -0.271, 0.5)
+    offset = Translation3d(-0.1525, -0.271, 0.5)
+    extreme = Translation3d(0.1525, -0.271, 0.5)
     red_hub = Translation3d(4.625594, 4.034536, 3.057144)
-    angle = computeRobotRotationToAlign(robot, offset, extrem, red_hub)
+    angle = computeRobotRotationToAlign(robot, offset, extreme, red_hub)
+    assert angle == approx((-27.0159828199 * math.pi / 180), abs=0.0001)
+
+
+def test_zero_angles():
+    robot = Pose3d(2, 3, 0, Rotation3d())
+    offset = Transform3d(-0.1525, -0.271, 0.5, Rotation3d())
+    extreme = Translation3d(0.1525, -0.271, 0.5)
+    red_hub = Translation3d(10, 3 - 0.271, 3.057144)
+    shooter_pose3d = robot.transformBy(offset)
+
+    angle = computeRobotRotationToAlign(robot, offset.translation(), extreme, red_hub)
     assert angle == approx(0.0, abs=0.0001)
+
+    angleSimple = computeRobotRotationToAlignSimple(shooter_pose3d, red_hub)
+    assert angleSimple == approx(0.0, abs=0.0001)
