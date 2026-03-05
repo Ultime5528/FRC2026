5cec620de2755e577b584235fe25f1d3786bf972
Charles R. <4201224@keranna.qc.ca>
2026-03-05 14:53:34 -0500

    Auto stash before merge of "integration-02-28" and "origin/integration-02-28"

--- modules/control.py
+++ modules/control.py
@@ -41,9 +41,15 @@ class ControlModule(Module):
         """
 
         # Intake
-        hardware.panel_1.povLeft().onTrue(MovePivot.toUp(hardware.pivot))
+<<<<<<< Updated upstream
+        hardware.panel_1.povLeft().onTrue(MovePivot.toDown(hardware.pivot))
 
+        hardware.panel_1.povRight().onTrue(MovePivot.toUp(hardware.pivot))
+=======
         hardware.panel_1.povRight().onTrue(MovePivot.toDown(hardware.pivot))
+>>>>>>> Stashed changes
+
+        hardware.panel_1.povLeft().onTrue(MovePivot.toUp(hardware.pivot))
 
         hardware.panel_1.povUp().whileTrue(GrabFuel(hardware.feeder))
 
