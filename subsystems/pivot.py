4c8547d42613179bed4d6bbe9eecb072b7341dfc
Étienne Beaulac <etienne.beaulac@outlook.com>
2026-03-09 18:03:31 -0400

    Fix Pivot

--- subsystems/pivot.py
+++ subsystems/pivot.py
@@ -21,10 +21,10 @@ from ultime.switch import Switch
 
 
 class Pivot(LinearSubsystem):
-    speed_maintain = autoproperty(100.0)
+    speed_maintain = autoproperty(0.01)
     min_position = autoproperty(0.0)
     max_position = autoproperty(5.0)
-    position_maintain_min = autoproperty(3.5)
+    position_maintain_min = autoproperty(-1.0)
     position_maintain_max = autoproperty(6.5)
 
     position_conversion_factor = autoproperty(1.0)
@@ -73,6 +73,7 @@ class Pivot(LinearSubsystem):
 
     def readInputs(self):
         self._encoder_position = self._encoder.getPosition()
+        self._motor_current_rpm = self._encoder.getVelocity()
         self._switch_max_pressed = self._switch_max.isPressed()
         self._switch_min_pressed = self._switch_min.isPressed()
 
