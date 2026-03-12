f995ace2e0aeece820e9f35d07306a711c3250ff
Charles Robitaille <4201224@keranna.qc.ca>
2026-03-12 16:22:15 -0400

    indexer higher speed

--- subsystems/shooter.py
+++ subsystems/shooter.py
@@ -27,9 +27,9 @@ class Shooter(Subsystem):
     flywheel_kS = autoproperty(0.119613)
     shooter_tolerance = autoproperty(30.0)
 
-    indexer_rpm = autoproperty(1200.0)
-    indexer_amplitude = autoproperty(800)
-    indexer_period = autoproperty(2.0)
+    indexer_rpm = autoproperty(1400.0)
+    indexer_amplitude = autoproperty(600)
+    indexer_period = autoproperty(1.0)
     indexer_rpm_stuck_threshold = autoproperty(50.0)
     indexer_rpm_unstuck = autoproperty(-200.0)
     indexer_delay_unstuck = autoproperty(2.0)
