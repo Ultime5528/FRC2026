797a3f04c908c8ba5e03a353f261215d748def12
Charles Robitaille <4201224@keranna.qc.ca>
2026-03-12 15:21:42 -0400

    Update Climber anti ballon en dessous

--- commands/autonomous/towerclimb.py
+++ commands/autonomous/towerclimb.py
@@ -9,6 +9,7 @@ from commands.drivetrain.aligntotower import AlignToTower
 from commands.drivetrain.auto.pathfindfollowpath import PathFindFollowPath
 from commands.drivetrain.driverelative import DriveRelative
 from commands.hugandclimb import HugAndClimb
+from commands.pivot.move import ManualMovePivot
 from modules.hardware import HardwareModule
 from ultime.autoproperty import autoproperty
 
@@ -33,6 +34,7 @@ class TowerClimb(SequentialCommandGroup):
         self.drivetrain = hardware.drivetrain
         self.climber = hardware.climber
         self.hugger = hardware.hugger
+        self.pivot = hardware.pivot
         self.path = path
 
         self.addCommands(
@@ -44,5 +46,6 @@ class TowerClimb(SequentialCommandGroup):
             deadline(
                 HugAndClimb(self.climber, self.hugger),
                 DriveRelative(self.drivetrain, lambda: Translation2d(-self.speed, 0.0)),
+                ManualMovePivot.up(self.pivot)
             ),
         )
